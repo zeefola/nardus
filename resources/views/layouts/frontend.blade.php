d4260349018cf671f29473ee180b15dc68acbf0c
zeefola <zeefola@gmail.com>
2020-08-18 11:14:55 +0100

    Removed meta tag

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="HTML5 Template" />
+    {{-- <meta name="keywords" content="HTML5 Template" />
     <meta name="description" content="Agrotek – Agriculture HTML Template" />
-    <meta name="author" content="https://www.themetechmount.com/" />
+    <meta name="author" content="https://www.themetechmount.com/" /> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title> @yield('title') </title>
 
