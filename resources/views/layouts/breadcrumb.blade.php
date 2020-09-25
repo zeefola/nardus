c83f5aaefaa5059f58a3f27160c0f96bf3bbf645
zeefola <zeefola@gmail.com>
2020-09-25 18:20:19 +0100

    breadcrumb markup

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -6,12 +6,17 @@
                     <div class="page-title-heading">
                         <h1 class="title">{{ $title }}</h1>
                     </div><!-- /.page-title-captions -->
-                    <div class="breadcrumb-wrapper">
-                        <span>
-                            <a title="Homepage" href="/"><i class="ti ti-home"></i>&nbsp;&nbsp;Home</a>
+                    <div class="breadcrumb-wrapper" itemscope itemtype="http://schema.org/BreadcrumbList">
+                        <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                            <a itemprop="item" title="Homepage" href="/"><i class="ti ti-home"></i>&nbsp;&nbsp; <span itemprop="name"> Home </span> </a>
+                            <meta itemprop="position" content="1" />
                         </span>
                         <span class="ttm-bread-sep ttm-textcolor-white">&nbsp; → &nbsp;</span>
-                        <span class="ttm-textcolor-skincolor">{{ $subtitle }}</span>
+                        <span class="ttm-textcolor-skincolor" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                           <a itemprop="item" href="{{ request()->fullUrl }}"> <span itemprop="name"> {{ $subtitle }}  /</span> </a>
+                            <meta itemprop="position" content="2" />
+                        </span>
+                        
                     </div>
                 </div>
             </div><!-- /.col-md-12 -->
